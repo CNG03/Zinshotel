57bb904536a83dd57ed2fcdef5429ce94b024fd7
Bui Cong <buivancng03@gmail.com>
2024-03-14 23:33:38 +0700

    xong trang home

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login_sign-up');
 });
 
+Route::get('sao', function () {
+    return view('home');
+});
