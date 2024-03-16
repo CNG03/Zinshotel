0b3fe421eae6fc99733931f3dd6302679c1be105
BanKhanhHocITNgu <21013041@st.phenikaa-uni.edu.vn>
2024-03-16 23:36:23 +0700

    cap nhat lai css

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Service</title>
     <link rel="stylesheet" href="./abc/fontawesome-free-5.15.4-web/fontawesome-free-5.15.4-web/css/all.min.css">
-    <link rel="stylesheet" href="service.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/service.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
