338aa5853b8eb04d814cfac064eef67ea10a444f
BanKhanhHocITNgu <21013041@st.phenikaa-uni.edu.vn>
2024-03-16 23:30:29 +0700

    update lai phan service

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -3,79 +3,150 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Document</title>
+    <link rel="stylesheet" href="./abc/fontawesome-free-5.15.4-web/fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="service.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="service">
         <header class="header">
-            <div class="grid">
-                <nav class="header__navbar">
-                    <ul class="header__nav-listLeft">
-                        <li class="header__nav-item">Ours Room</li>
-                        <li class="header__nav-item">Service</li>
-                    </ul>
-                    <a href="#home" class="header__nav-list">
-                        <img src="#logo" alt="" class="container-img">
-                    </a>
-                    <ul class="header__nav-listRight">
-                        <li class="header__nav-item"><i class="fas fa-search"></i>Search</li>
-                        <li class="header__nav-item">
-                            <a href="" class="header__nav-icon-link">
-                                <i class="fas fa-user"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+        <!-- Đợi bổ sung -->
         </header>
-
         <div class="container">
-            <div class="container__content1">
-                <div class="grid">
-                    <div class="grid-container">
-                        <div class="grid-container-item3">
-                            <h3 class="container-content">
-                                Khám phá biển cả trong sự sang trọng của chúng tôi. Du thuyền 5 sao - Trải nghiệm du lịch hoàn hảo. Khám phá biển cả, tận hưởng hạnh phúc không giới hạn.
-                            </h3>
-                            <img src="https://geos-nature.org/wp-content/uploads/2021/12/9-2.jpg" alt="" class="container-img">
-                        </div>
+            <section class="du-thuyen">
+                <div class="col-md-6 spa-ads du-thuyen-text">
+                    <h1>Rent A Yacht</h1>
+                    <p>
+                        Explore the sea in our luxury. 5-star cruise - Perfect travel experience. Explore the sea, enjoy unlimited happiness.
+                    </p>
+                    <div class="book-spa-session view-more">
+                        <a href="">
+                            <button type="button" class="btn">More</button>
+                        </a>
                     </div>
                 </div>
-            </div>
-            <div class="container__content2">
-                    <div class="grid">
-                        <div class="grid-container">
-                            <div>
-                                <img src="https://media.baodautu.vn/Images/chicuong/2021/09/29/16.jpg" alt="" class="container-img-content2">
-                            </div>
-                            <div class="container-content">
-                                <h2>Sống xanh - Yêu thiên nhiên</h2>
-                                <p>
-                                    "Hòa mình vào cảnh quan hữu tình, du khách được tận hưởng không gian xanh mát.Hãy cùng chúng tôi khám phá vẻ đẹp tự nhiên tuyệt vời và tạo dấu ấn cho một tương lai xanh hơn."
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-            </div>
+                <div style="margin-top: 50px;" class="duthuyen-img col-md-6">
+                    <img src="https://geos-nature.org/wp-content/uploads/2021/12/9-2.jpg" alt="">
+                </div>
+            </section>
 
-            <div class="container__content3">
-                <div class="grid">
-                    <div class="grid-container">
-                        <div class="grid-container-item3">
-                            <p class="grid-container-content">
-                                Wellness Center
-Indulge in a wide array of treatments and rituals designed to cater to your unique needs and preferences. From soothing massages to revitalizing facials, invigorating body scrubs to detoxifying wraps, our spa menu offers a diverse range of services that promise to leave you feeling refreshed, renewed, and invigorated.
-                            </p>
-                            <img src="https://thacobus.vn/storage/bus/chi-tiet-sp/xe-bus-facelift/mobihome-luxury/banner-new-mobihome-luxury-sp.jpg" alt="" class="container-img">
-                        </div>
+            <section class="spa-ads travel-tour">
+                <div style="margin-top: 50px;" class="travel-tour-img">
+                    <div style="display: flex; justify-content: center; align-items: center;">
+                        <img style="width: 100%; align-content: center;" src="https://media.baodautu.vn/Images/chicuong/2021/09/29/16.jpg" alt="">
                     </div>
                 </div>
-            </div>
-            <div class="container__content4"></div>
-            <div class="container__content5"></div>
-            <div class="container__content6"></div>
-            <div class="container__content7"></div>
+            </section>
+
+            <section class="spa-ads travel-tour">
+                <div class="travel-tour-text">
+                    <h1>Travel Tours</h1>
+                    <p>
+                        Immersed in the charming landscape, visitors can enjoy the cool green space.
+                        <br>Join us to explore amazing natural beauty and make your mark for a greener future.
+                    </p>
+                </div>
+                <div class="book-spa-session view-more">
+                    <a href="">
+                        <button type="button" class="btn">More</button>
+                    </a>
+                </div>
+            </section>
+
+            <section class="bus">
+                <div style="margin-top: 50px;" class="bus-img col-md-6">
+                    <img src="https://thacobus.vn/storage/bus/chi-tiet-sp/xe-bus-facelift/mobihome-luxury/banner-new-mobihome-luxury-sp.jpg" alt="">
+                </div>
+                <div class="col-md-6 spa-ads bus-text">
+                    <h1>Bus Tours</h1>
+                    <p>
+                        Featuring top-notch buses, premium amenities, and professional service, we ensure a journey of sophistication and comfort, allowing guests to explore the world in style and grandeur.
+                    </p>
+                    <div class="book-spa-session view-more">
+                        <a href="">
+                            <button type="button" class="btn">More</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section style="margin-top: 50px;" class="bonus-service">
+                <div class="row" style="width: 100%;">
+                    <div class="service-item col-md-2";>
+                        <a class="service-style" href="">
+                            <div class="service-img">
+                                <img src="https://media.istockphoto.com/id/1351326672/vi/anh/nh%C3%ACn-v%E1%BB%81-ph%C3%ADa-nam-d%E1%BB%8Dc-theo-s%C3%B4ng-thames-t%E1%BB%AB-c%C3%A1p-treo-tr%C3%AAn-cao-emirates-air-line-london-anh-v%C6%B0%C6%A1ng.jpg?s=612x612&w=0&k=20&c=REmaMQey14BSDIPnDeLfDflzah1IiZpG6siPdFb8CKc=" alt="">
+                            </div>
+                            <div class="learn-more">
+                                <button type="button" class="btn">More</button>
+                            </div>
+                            <div class="service-info-text">
+                                <h3>Cable Car</h3>
+                                <p>đ/Person</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="service-item col-md-3";>
+                        <a class="service-style" href="">
+                            <div class="service-img">
+                                <img src="https://nhahangso.com/wp-content/uploads/2023/02/khach-hang-cua-quan-nuong-ngoai-troi.webp" alt="">
+                            </div>
+                            <div class="learn-more">
+                                <button type="button" class="btn">More</button>
+                            </div>
+                            <div class="service-info-text">
+                                <h3>BBQ</h3>
+                                <p>đ/Family</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="service-item col-md-3";>
+                        <a class="service-style" href="">
+                            <div class="service-img">
+                                <img src="https://media.istockphoto.com/id/200119694-002/vi/anh/female-kiteboarder-in-mid-air-looking-over-shoulder-low-angle-view.jpg?s=612x612&w=0&k=20&c=qG1pvmc2h1F45uB9u2voWC9w8aL38ACybncpB_Q-sYA=" alt="">
+                            </div>
+                            <div class="learn-more">
+                                <button type="button" class="btn">More</button>
+                            </div>
+                            <div class="service-info-text">
+                                <h3>Kitesurfing</h3>
+                                <p>đ/Person</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="service-item col-md-2";>
+                        <a class="service-style" href="">
+                            <div class="service-img">
+                                <img src="https://luhanhvietnam.com.vn/du-lich/vnt_upload/news/03_2023/kinh-nghiem-di-thuy-phi-co-ha-long-quang-ninh-_.jpg" alt="">
+                            </div>
+                            <div class="learn-more">
+                                <button type="button" class="btn">More</button>
+                            </div>
+                            <div class="service-info-text">
+                                <h3>Seaplane</h3>
+                                <p>đ/hour</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="service-item col-md-2";>
+                        <a class="service-style" href="">
+                            <div class="service-img">
+                                <img src="https://www.kkday.com/vi/blog/wp-content/uploads/nui-chua-chan-2-min.jpg" alt="">
+                            </div>
+                            <div class="learn-more">
+                                <button type="button" class="btn">More</button>
+                            </div>
+                            <div class="service-info-text">
+                                <h3>Seaplane</h3>
+                                <p>đ/hour</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
         <footer class="footer"></footer>
     </div>
